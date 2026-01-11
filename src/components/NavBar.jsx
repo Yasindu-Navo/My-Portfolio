0cf0b02f5938d3333c8208b2644bc0863b1f9203
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-11 15:36:12 +0530

    Fix section IDs in AboutSection and NavBar components for consistency; refactor HeroSection for improved readability

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,11 @@ import { Menu, X } from "lucide-react";
 
 
 const navItems = [
-    { name: "Home", href: "#hero" },
-    { name: "About", href: "#About" },
-    { name: "Skills", href: "#Skills" },
-    { name: "Projects", href: "#Projects" },
-    { name: "Contact", href: "#Contact" },
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
 ];
 
 
