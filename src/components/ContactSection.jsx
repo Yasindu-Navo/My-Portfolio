2c4f0d5d811312eaccd196aeaeca91b628f614a9
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-12 11:14:18 +0530

    Add Contact Information section to ContactSection component for improved user connectivity

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,3 +1,4 @@
+import { Mail } from "lucide-react";
 import React from "react";
 
 function ContactSection() {
@@ -11,7 +12,45 @@ function ContactSection() {
         <p className=" text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
           Looking for a passionate developer to join your team? I’m always open
           to new opportunities, collaborations, and meaningful conversations.
-        </p>
+              </p>
+              
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                  
+                  <div className="space-y-8">
+                      <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+
+
+                      <div className="space-y-6 justify-center">
+                          <div className="flex items-start space-x-4">
+                              <div className="p-3 rounded-full  bg-primary/10 ">
+                                  <Mail className="w-6 h-6 text-primary"/>
+                              </div>
+                              <div>
+                              <h4 className="font-medium">Email</h4>
+                              <a
+                                  href="mailto:yasindunavodh06@gmail.com"
+                                  className="text-muted-foreground hover:text-primary transition-colors"
+                              >
+                                  yasindunavodh06@gmail.com
+                                  </a>
+                                  </div>
+                          </div>
+
+                      </div>
+                  </div>
+
+                  
+
+
+
+
+             
+                  
+        </div>
+
+
+
       </div>
     </section>
   );
