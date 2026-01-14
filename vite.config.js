9d47def1ce6459c737bd761213faa5c90f471885
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-14 15:27:03 +0530

    Add GitHub Actions workflow for deploying to GitHub Pages and configure CNAME

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": "/src",
-    }
-  }
-})
+    },
+  },
+});
