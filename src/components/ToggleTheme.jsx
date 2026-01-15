34f0dd488baaf076c10b9e16ab0d2f0e530a5e33
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-15 11:21:37 +0530

    Refactor button formatting for improved readability

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -30,14 +30,13 @@ export const ToggleTheme = () => {
     }
   };
   return (
-    <button 
+    <button
       onClick={toggleTheme}
       className={cn(
         "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700",
         "focus:outline-hidden"
       )}
     >
-
       {isDarkTheme ? (
         <Sun className="h-6 w-6 text-yellow-300" />
       ) : (
